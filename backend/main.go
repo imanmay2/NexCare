c53c94fb6108ddaa9cad9c6cd1b279280817256b
manmay2 <imanmay2@gmail.com>
2026-01-30 22:45:20 +0530

    database connection done

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"log"
 	gin "github.com/gin-gonic/gin"
+	"os"
+	"nexcare/backend/config"
+	env "github.com/joho/godotenv"
+
 )
 
 func main() {
+	env.Load()
 	log.Println("Welcome to NexCare")
+
+	log.Printf("----> PostgreSQL : %v\n",os.Getenv("DB_URL"));
+	config.ConnectDB()
+	
 
 	app:=gin.Default()
 
 	app.GET("/",func(ctx *gin.Context){
 		ctx.IndentedJSON(200,gin.H{"Message":"Welcome to NexCare","success":"true"})
 	})
+
 
 
 	app.Run(":8090");
